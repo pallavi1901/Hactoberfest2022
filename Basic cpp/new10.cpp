6d3a9ed3d5b5673326a175af95d4f2167574b4a9
pallavi1901 <92847182+pallavi1901@users.noreply.github.com>
2022-10-02 22:22:45 +0530

    Add files via upload

---
+++ Basic cpp/new10.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int n, i, j;
+    cout<<"enter the no of rows: "<<endl;
+    cin>>n;
+
+    int space = n-1;
+
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=space; j++)
+        {
+            cout<<" ";
+        }
+        space--;
+        
+        for(j=1; j<=i; j++)
+        {
+            cout<<i<<" ";
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}
