1192ca1f156f41e115462db4420419c8cbe4bbbf
pallavi1901 <92847182+pallavi1901@users.noreply.github.com>
2022-10-05 19:31:13 +0530

    Update pattern_rect.cpp

--- Basic cpp/pattern_rect.cpp
+++ Basic cpp/pattern_rect.cpp
@@ -13,22 +13,7 @@ int main()
         {
             cout<<" ";
         }
-        for(int j = 1; j<=n; j++)
-        {
-            if(i==1 || i==n )
-            {
-                cout<<'*';
-            }
-            else if( j==1 || j==n)
-            {
-                cout<<'*';
-            }
-            else
-            {
-                cout<<" ";
-            }
-        }
-
+       
         cout<<endl;
     }
     return 0;
