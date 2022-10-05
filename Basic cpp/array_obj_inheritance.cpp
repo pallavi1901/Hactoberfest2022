1969295b14c0b32107fd04b3e884dc59f295bcdd
pallavi1901 <92847182+pallavi1901@users.noreply.github.com>
2022-10-05 19:34:25 +0530

    Add files via upload

---
+++ Basic cpp/array_obj_inheritance.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#define Max 100
+using namespace std;
+
+class student
+{                                               //array object
+    int rno, total, per;
+    char name[20];
+    int m1, m2, m3;
+
+    public:
+         void input();
+         void print();
+
+};
+void student::input()
+{
+    cout<<"enter roll call, name, marks1, marks2, marks3: "<<endl;
+    cin>>rno>>name>>m1>>m2>>m3;
+}
+void student::print()
+{
+    cout<<"roll no: "<<rno<<endl;
+    cout<<"name: "<<name<<endl;
+    cout<<"marks1: "<<m1<<endl;
+    cout<<"marks2: "<<m2<<endl;
+    cout<<"marks3: "<<m3<<endl;
+
+    total = m1+m2+m3;
+    per = total/3;
+
+    cout<<"total: "<<total<<endl;
+    cout<<"percentage: "<<per<<endl;
+}
+int main()
+{
+    int i, n;
+
+    cout<<"enter how many students info you want to print: "<<endl;
+    cin>>n;
+    
+    student obj [Max];
+    for(i=1; i<=n; i++)
+    {
+        obj[i].input();
+    }
+    for(i=1; i<=n; i++)
+    {
+        obj[i].print();
+    }
+    return 0;
+}
